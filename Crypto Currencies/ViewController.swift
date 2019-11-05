a1435d688d06f7f67172b377bee26e0291093d8e
Brian Corrieri <brian.corrieri@gmail.com>
2019-11-05 18:37:29 +0100

    Initial Commit

---
+++ Crypto Currencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crypto Currencies
+//
+//  Created by Brian Corrieri on 05/11/2019.
+//  Copyright © 2019 FairTrip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
